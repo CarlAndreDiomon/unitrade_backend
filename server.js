835a3c3f093083f69b033337dd89ced019f0d45b
CarlAndreDiomon <dcarlandrerafols@gmail.com>
2026-01-05 03:26:24 +0800

    add frontend origin

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://carlandrediomon.github.io/UniTrade',
   credentials: true,
 }));
 app.use(express.json());
